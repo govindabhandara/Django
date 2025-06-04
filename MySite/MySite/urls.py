c838cad875471796a932ee153f17ab0618274471
govindabhandara <gvndbhandara@gmail.com>
2025-06-04 11:50:57 +0530

    Django

--- MySite/MySite/urls.py
+++ MySite/MySite/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myuserapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('wish/',views.wish),
+    path('index/',views.index),
+    path('about/',views.service),
+    path('service',views.service)
+
 ]
--- MySite/myuserapp/apps.py
+++ MySite/myuserapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserappConfig(AppConfig):
+class MyuserappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userapp'
+    name = 'myuserapp'
